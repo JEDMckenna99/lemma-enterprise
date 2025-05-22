becdca4bb17ae2182e7a69e4d32c3b518d95aa90
Jed McKenna <jedmckenna99@gmail.com>
2025-05-22 18:01:20 -0400

    Fix deployment configuration for OPRF service

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn wsgi:app
-oprf: ./bin/oprfservice --port=${PORT} --keydir=./instance/data/keys
+oprf: ./bin/oprfservice --port=$PORT --keydir=./instance/data/keys
--- bin/compile-oprf.sh
+++ bin/compile-oprf.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Build the OPRF service from the oprfservice directory
+echo "Compiling OPRF service..."
 cd oprfservice
 go build -o ../bin/oprfservice
-
-# Make the binary executable
 chmod +x ../bin/oprfservice
-
-echo "OPRF service compiled successfully" 
+echo "OPRF service compiled successfully"
