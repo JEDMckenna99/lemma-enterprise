aa56bab407c44735db0ead3121b951bffd5d2753
Jed McKenna <jedmckenna99@gmail.com>
2025-05-22 20:17:24 -0400

    Update build script to install Go for OPRF compilation

--- bin/heroku-build.sh
+++ bin/heroku-build.sh
@@ -6,6 +6,13 @@ echo "=== Starting Heroku build process ==="
 # Make scripts executable
 chmod +x bin/*.sh
 
+# Install Go for OPRF service
+echo "=== Installing Go ==="
+curl -L https://go.dev/dl/go1.18.linux-amd64.tar.gz -o go.tar.gz
+tar -C /tmp -xzf go.tar.gz
+export PATH=$PATH:/tmp/go/bin
+go version
+
 # Compile OPRF service
 echo "=== Compiling OPRF service ==="
 ./bin/compile-oprf.sh
