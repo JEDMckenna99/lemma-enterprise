fd40b53f4a06c665284f902ade549505220449b6
Jed McKenna <jedmckenna99@gmail.com>
2025-05-28 21:41:51 -0400

    Add proper Go cmd structure for multi-binary build with OPRF service

--- main.go
+++ main.go
@@ -1,44 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Building OPRF service...")
-
-	// Create bin directory
-	binDir := "bin"
-	if err := os.MkdirAll(binDir, 0755); err != nil {
-		fmt.Printf("Failed to create bin directory: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Build the OPRF service from oprfservice directory
-	oprfDir := "oprfservice"
-	outputPath := filepath.Join(binDir, "oprf-service")
-
-	fmt.Printf("Building OPRF service from %s to %s\n", oprfDir, outputPath)
-
-	cmd := exec.Command("go", "build", "-o", outputPath, "./oprfservice")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Printf("Failed to build OPRF service: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("OPRF service built successfully!")
-
-	// Make the binary executable
-	if err := os.Chmod(outputPath, 0755); err != nil {
-		fmt.Printf("Failed to make binary executable: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Build complete!")
+	fmt.Println("Lemma Enterprise - Multi-binary Go project")
+	fmt.Println("OPRF service binary should be built in cmd/oprf-service/")
 }
