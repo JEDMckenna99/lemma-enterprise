3cf07dfc3233201fefaf5c8656846eafe515c18d
Surv Admin <jedmckenna@surv.com>
2026-02-04 09:58:31 -0500

    Allow cross-platform passkey providers like Google Password Manager

--- static/js/lemma-wallet.js
+++ static/js/lemma-wallet.js
@@ -1497,7 +1497,6 @@ class LemmaWallet {
                     { alg: -257, type: 'public-key' }  // RS256
                 ],
                 authenticatorSelection: {
-                    authenticatorAttachment: 'platform',
                     userVerification: 'required',
                     residentKey: 'preferred'
                 },
